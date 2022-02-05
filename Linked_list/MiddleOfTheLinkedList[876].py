bc35472ee81d306e30cc0f4e0d3b264f104818aa
aditya <aditya.1923it1006@kiet.edu>
2022-02-05 20:54:22 +0530

    commit

---
+++ Linked_list/MiddleOfTheLinkedList[876].py
@@ -0,0 +1,28 @@
+#Given the head of a singly linked list, return the middle node of the linked list.
+
+#If there are two middle nodes, return the second middle node.
+
+#Example 1:
+
+#Input: head = [1,2,3,4,5]
+#Output: [3,4,5]
+#Explanation: The middle node of the list is node 3.
+#Example 2:
+
+#Input: head = [1,2,3,4,5,6]
+#Output: [4,5,6]
+#Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+#CODE
+
+class Solution(object):
+    def middleNode(self, head):
+        fast , slow = head.next, head
+        if not fast:
+            return slow
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        x = slow.next
+        slow.next = None
+        return x
